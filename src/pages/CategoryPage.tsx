34dc34e8e326bb0df1d8f6d9bd9f82ee6977b804
Duta <dutaalif.20091@mhs.unesa.ac.id>
2025-07-02 11:34:35 +0700

    Fixing category page

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,61 @@
-import { Swiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {  useEffect, useState } from "react";
+import { Category, HomeService } from "../types/type";
+import apiClient from "../services/apiServices";
+import { Link, useParams } from "react-router-dom";
 
+const fetchServices = async () => {
+  const response = await apiClient.get("/services");
+  return response.data.data;
+};
 
 export default function CategoryPage() {
+  const [services, setServices] = useState<HomeService[]>([]);
+  const [loadingServices, setLoadingServices] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const { slug } = useParams<{ slug: string }>();
+  const [category, setCategory] = useState<Category | null>(null); 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    apiClient
+    .get(`/category/${slug}`)
+    .then((response) => {
+      setCategory(response.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError(error);
+      setLoading(false);
+    });
+  }, [slug]);
+
+  useEffect(() => {
+    const fetchServicesData = async () => {
+      try {
+        const servicesData = await fetchServices();
+        setServices(servicesData);
+      } catch {
+        setError("Failed to fetch services");
+      } finally {
+        setLoadingServices(false);
+      }
+    };
+
+    fetchServicesData();
+  }, []);
+
+  const BASE_URL = import.meta.env.VITE_REACT_API_STORAGE_URL;
+
+  const filteredServices = slug
+    ? services.filter(
+        (service) => service.category.slug.toLowerCase() === slug.toLowerCase()
+      )
+    : services;
+
+  if (loadingServices) return <p>Loading services...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <main className="relative mx-auto flex w-full max-w-[640px] flex-col gap-[30px] overflow-x-hidden bg-white pb-[112px]">
       <div id="Background" className="absolute left-0 right-0 top-0">
@@ -20,7 +74,7 @@ export default function CategoryPage() {
             id="ContainerNav"
             className="flex items-center justify-between py-[14px] transition-all duration-300"
           >
-            <a href="index.html">
+            <Link to={"/"}>
               <div
                 id="Back"
                 className="flex h-[44px] w-[44px] shrink-0 items-center justify-center rounded-full bg-white"
@@ -31,7 +85,7 @@ export default function CategoryPage() {
                   className="h-[22px] w-[22px] shrink-0"
                 />
               </div>
-            </a>
+            </Link>
             <h2
               id="Title"
               className="font-semibold text-white transition-all duration-300"
@@ -61,9 +115,9 @@ export default function CategoryPage() {
         />
         <div>
           <h1 className="text-[26px] font-extrabold leading-[39px] text-white">
-            Living Room
+            {slug}
           </h1>
-          <p className="text-white">189,394 Services</p>
+          <p className="text-white">{category?.home_services_count ?? 0} Services</p>
         </div>
       </header>
       <section id="mostOrdered" className="relative space-y-[14px]">
@@ -71,61 +125,75 @@ export default function CategoryPage() {
           Most Ordered
         </h3>
         <div id="MostOrderedSlider" className="swiper w-full overflow-x-hidden">
-          <Swiper className="swiper-wrapper">
-            <div className="swiper-slide !w-fit">
-              <a href="service-details.html" className="card">
-                <div className="relative flex w-[230px] shrink-0 flex-col gap-[12px] overflow-hidden rounded-[24px] border border-shujia-graylight bg-white p-4 transition-all duration-300 hover:border-shujia-orange">
-                  <span className="absolute right-[26px] top-[26px] shrink-0 rounded-full bg-white px-2 py-[7px]">
-                    <div className="flex items-center gap-[2px]">
-                      <img src="/assets/images/icons/star.svg" alt="icon" />
-                      <p className="text-xs font-semibold leading-[18px]">
-                        4.8
-                      </p>
-                    </div>
-                  </span>
-                  <div className="flex h-[140px] w-full shrink-0 items-center justify-center overflow-hidden rounded-[16px] bg-[#D9D9D9]">
-                    <img
-                      src="/assets/images/thumbnails/cleaning-medium.png"
-                      alt="image"
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
-                  <h4 className="line-clamp-2 min-h-[48px] font-semibold">
-                    Mini Swim Pool and Kids Soccer Fields Grass
-                  </h4>
-                  <div className="flex flex-col gap-y-3">
-                    <div className="flex items-center gap-2">
-                      <img
-                        src="/assets/images/icons/date.svg"
-                        alt="icon"
-                        className="h-5 w-5 shrink-0"
-                      />
-                      <p className="text-sm leading-[21px] text-shujia-gray">
-                        Living Room
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <img
-                        src="/assets/images/icons/clock.svg"
-                        alt="icon"
-                        className="h-5 w-5 shrink-0"
-                      />
-                      <p className="text-sm leading-[21px] text-shujia-gray">
-                        15 Hours
-                      </p>
-                    </div>
-                    <strong className="font-semibold text-shujia-orange">
-                      Rp 8.560.392
-                    </strong>
-                    <img
-                      className="absolute bottom-0 right-0"
-                      src="/assets/images/backgrounds/decoration.svg"
-                      alt="icon"
-                    />
-                  </div>
-                </div>
-              </a>
-            </div>
+          <Swiper
+            className="swiper-wrapper"
+            direction="horizontal"
+            spaceBetween={20}
+            slidesPerView="auto"
+            slidesOffsetAfter={20}
+            slidesOffsetBefore={20}
+          >
+            {filteredServices.length > 0
+              ? filteredServices.map((service) => (
+                  <SwiperSlide className="swiper-slide !w-fit" key={service.id}>
+                    <a href="service-details.html" className="card">
+                      <div className="relative flex w-[230px] shrink-0 flex-col gap-[12px] overflow-hidden rounded-[24px] border border-shujia-graylight bg-white p-4 transition-all duration-300 hover:border-shujia-orange">
+                        <span className="absolute right-[26px] top-[26px] shrink-0 rounded-full bg-white px-2 py-[7px]">
+                          <div className="flex items-center gap-[2px]">
+                            <img
+                              src="/assets/images/icons/star.svg"
+                              alt="icon"
+                            />
+                            <p className="text-xs font-semibold leading-[18px]">
+                              4.8
+                            </p>
+                          </div>
+                        </span>
+                        <div className="flex h-[140px] w-full shrink-0 items-center justify-center overflow-hidden rounded-[16px] bg-[#D9D9D9]">
+                          <img
+                            src={`${BASE_URL}/${service.thumbnail}`}
+                            alt="image"
+                            className="h-full w-full object-cover"
+                          />
+                        </div>
+                        <h4 className="line-clamp-2 min-h-[48px] font-semibold">
+                          {service.name}
+                        </h4>
+                        <div className="flex flex-col gap-y-3">
+                          <div className="flex items-center gap-2">
+                            <img
+                              src="/assets/images/icons/date.svg"
+                              alt="icon"
+                              className="h-5 w-5 shrink-0"
+                            />
+                            <p className="text-sm leading-[21px] text-shujia-gray">
+                              {service.category.name}
+                            </p>
+                          </div>
+                          <div className="flex items-center gap-2">
+                            <img
+                              src="/assets/images/icons/clock.svg"
+                              alt="icon"
+                              className="h-5 w-5 shrink-0"
+                            />
+                            <p className="text-sm leading-[21px] text-shujia-gray">
+                              {service.duration} Hours
+                            </p>
+                          </div>
+                          <strong className="font-semibold text-shujia-orange">
+                            Rp {service.price.toLocaleString("id-ID")}
+                          </strong>
+                          <img
+                            className="absolute bottom-0 right-0"
+                            src="/assets/images/backgrounds/decoration.svg"
+                            alt="icon"
+                          />
+                        </div>
+                      </div>
+                    </a>
+                  </SwiperSlide>
+                ))
+              : "Belum ada service yang tersedia."}
           </Swiper>
         </div>
       </section>
@@ -137,35 +205,44 @@ export default function CategoryPage() {
           <h3 className="pl-5 text-[18px] font-bold leading-[27px]">
             Latest Services
           </h3>
-          <a href="service-details.html">
-            <div className="mx-5 flex gap-3 rounded-[24px] border border-shujia-graylight bg-white p-4 transition-all duration-300 hover:border-shujia-orange">
-              <div className="flex h-[101px] w-[120px] shrink-0 items-center justify-center overflow-hidden rounded-2xl">
-                <img
-                  src="/assets/images/thumbnails/write-mini.png"
-                  alt="image"
-                  className="h-full w-full object-cover"
-                />
-              </div>
-              <div className="flex flex-col gap-1">
-                <h4 className="line-clamp-2 min-h-[48px] font-semibold">
-                  Home Family Theater Best Lights Installation
-                </h4>
-                <div className="flex items-center gap-2">
-                  <img
-                    src="/assets/images/icons/clock.svg"
-                    alt="icon"
-                    className="h-5 w-5 shrink-0"
-                  />
-                  <p className="text-sm leading-[21px] text-shujia-gray">
-                    15 Hours
-                  </p>
+          {services.length > 0 ? (
+            services.map((service) => (
+              <Link key={service.id} to={`/service-details/${service.id}`}>
+                <div className="mx-5 flex gap-3 rounded-[24px] border border-shujia-graylight bg-white p-4 transition-all duration-300 hover:border-shujia-orange">
+                  <div className="flex h-[101px] w-[120px] shrink-0 items-center justify-center overflow-hidden rounded-2xl">
+                    <img
+                      src={`${BASE_URL}/${service.thumbnail}`}
+                      alt="image"
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+
+                  <div className="flex flex-col gap-1">
+                    <h4 className="line-clamp-2 min-h-[48px] font-semibold">
+                      {service.name}
+                    </h4>
+                    <div className="flex items-center gap-2">
+                      <img
+                        src="/assets/images/icons/clock.svg"
+                        alt="icon"
+                        className="h-5 w-5 shrink-0"
+                      />
+                      <p className="text-sm leading-[21px] text-shujia-gray">
+                        {service.duration} Hours
+                      </p>
+                    </div>
+                    <strong className="font-semibold text-shujia-orange">
+                      Rp {service.price.toLocaleString("id-ID")}
+                    </strong>
+                  </div>
                 </div>
-                <strong className="font-semibold text-shujia-orange">
-                  Rp 8.560.392
-                </strong>
-              </div>
-            </div>
-          </a>
+              </Link>
+            ))
+          ) : (
+            <p className="pl-5 text-shujia-gray">
+              No services found for this category.
+            </p>
+          )}
         </div>
       </section>
       <section
